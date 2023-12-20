8d4c5220835e7451e4e3c55a1bd14d4b5bec94ac
enovix_jjung <jaegunjung>
2023-12-20 09:40:28 -0800

    Store the Annual BTC analysis to a table

--- manual_utils/analysis_BTC.sql
+++ manual_utils/analysis_BTC.sql
@@ -2,6 +2,7 @@
 First Halving: November 28, 2012
 Second Halving: July 9, 2016
 Third Halving: May 11, 2020
+Fourth Halving: Apr 19, 2024
 ******/
 
 drop table #btc_yr
@@ -13,7 +14,8 @@ FROM [cda].[dbo].[CryptoDaily]
 WHERE SUBSTRING(FORMAT([Date], 'MM-dd'), 1, 5) = '01-01'
 
 -- Calculate APR
-select Date, Open_Price_USD, Yr_end_Price, (Yr_end_Price/Open_Price_USD -1) * 100 as APR_per
+select FORMAT([Date], 'yyyy') as [Year], Open_Price_USD, Yr_end_Price, (Yr_end_Price/Open_Price_USD -1) * 100 as APR_per
+into Anal_ann_BTC
 from
 (select Date, Open_Price_USD, lead(Open_Price_USD) over (order by Date) as Yr_end_Price 
 from #btc_yr) a
