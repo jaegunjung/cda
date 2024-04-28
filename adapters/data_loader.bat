34db1698f6bdde956d5315fb0cd98d6df59d1418
Jaegun Jung <jjg0412@gmail.com>
2024-04-27 21:41:06 -0700

    batch script to automate data loading

---
+++ adapters/data_loader.bat
@@ -0,0 +1,5 @@
+cd D:\github\cda
+D:
+"D:\github\cda\.venv\Scripts\python.exe" "D:\github\cda\adapters\stock_adapter.py"
+"D:\github\cda\.venv\Scripts\python.exe" "D:\github\cda\adapters\coingecko_data_adapter.py"
+"D:\github\cda\.venv\Scripts\python.exe" "D:\github\cda\adapters\changelly_forecast_adapter.py"
