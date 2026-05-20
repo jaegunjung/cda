bc41afd626793a1bc63c0314b1ca9bc8570e50d9
Jaegun Jung <jjg0412@gmail.com>
2026-05-19 18:15:59 -0700

    Changed the column format to host PEPE whose price is less than a cent

--- sql_scripts/CryptoDaily.sql
+++ sql_scripts/CryptoDaily.sql
@@ -8,9 +8,9 @@ CREATE TABLE  {TBLNAME} (
     ID int IDENTITY(1,1) primary key,
 	Crypto nvarchar(50) NOT NULL,
 	[Date] datetimeoffset (7) NOT NULL,
-	Open_Price_USD decimal(20,2) NOT NULL,
-	Total_Volume_USD decimal(30,2) NOT NULL,
-	Market_Cap_USD decimal(38,2) NOT NULL,
+	Open_Price_USD decimal(28,18) NOT NULL,
+	Total_Volume_USD decimal(38,18) NOT NULL,
+	Market_Cap_USD decimal(38,18) NOT NULL,
 	DateTmModified datetimeoffset (7) default SYSDATETIMEOFFSET(),
 )
 CREATE UNIQUE INDEX uidx_CryptoDaily on {TBLNAME} (Crypto, [Date]);
