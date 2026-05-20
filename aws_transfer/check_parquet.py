6df41efa6384694bcd157372639cedac044ae689
Jaegun Jung <jjg0412@gmail.com>
2026-05-19 18:13:05 -0700

    Checking crypto_data.pqrquet

--- aws_transfer/check_parquet.py
+++ aws_transfer/check_parquet.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-file = "export\market\year=2026\market_data.parquet"
+file = "export\market\crypto\year=2026\crypto_data.parquet"
 df = pd.read_parquet(file)
 print(df.head())
